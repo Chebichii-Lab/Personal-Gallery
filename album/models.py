e3e7c8b986b1f978005fb0d181814565fd7fa427
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-02 17:29:18 +0300

    add image class in the models.py

--- album/models.py
+++ album/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Image class
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
