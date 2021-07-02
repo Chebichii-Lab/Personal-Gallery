8c4a24404c7b607c30f67f24a3a9107c37d89dac
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-02 20:17:38 +0300

    add methods for location in the models.py file

--- album/models.py
+++ album/models.py
@@ -21,6 +21,24 @@ class Category(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
 
 # Image class
 class Image(models.Model):
