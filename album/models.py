75bfabd6fa66772bbc350ea31e18f1638583ce44
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-02 17:40:28 +0300

    add category class in the models.py file

--- album/models.py
+++ album/models.py
@@ -10,3 +10,9 @@ class Image(models.Model):
     # category = models.ForeignKey()
     # location = models.ForeignKey()
 
+# Category class 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+
