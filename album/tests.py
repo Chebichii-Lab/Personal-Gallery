894ac7e05a817627c1c560e37d52cda1b61ee279
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-04 12:42:37 +0300

    add Test of category class

--- album/tests.py
+++ album/tests.py
@@ -1,3 +1,18 @@
+from album.models import Category
 from django.test import TestCase
 
 # Create your tests here.
+
+#Test Category:
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name='food')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0) 
